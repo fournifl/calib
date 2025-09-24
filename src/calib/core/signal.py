5942a938f61de1d0c11db8f3233832f6ee768bea
Florent <florent.fournier@wavesnsee.com>
2025-09-24 15:28:51 +0200

    signal script

---
+++ src/calib/core/signal.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def normalize(array, low=0.0, high=255.0):
+    """Normalize an array between `min` and `max
+
+    Input array
+    Parameters
+    ----------
+    array : np.ndarray
+        a numpy array, to be normalized
+    low : float, int
+        minimum value of returned array
+    high : float, int
+        maximum value of returned array
+
+    Returns
+    -------
+    normalized : np.ndarray
+        the data from `array`, normalized between `min` and `max`
+    """
+    old_min = np.nanmin(array)
+    old_max = np.nanmax(array)
+    if old_min == old_max:
+        factor = 1
+    else:
+        factor = high / (old_max - old_min)
+    if np.ma.isMaskedArray(array):
+        normalized = np.ma.zeros(array.shape)
+        normalized.mask = array.mask
+        normalized[~array.mask] = (array[~array.mask] - old_min) * factor + low
+    else:
+        normalized = (array - old_min) * factor + low
+    return normalized
